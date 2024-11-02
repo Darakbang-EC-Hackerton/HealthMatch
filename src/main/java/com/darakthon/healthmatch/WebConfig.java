0a4ab15aada21fd49003cc26c6fd8ff6159c90fd
DawnGlow <ferrater1013@gmail.com>
2024-11-03 05:21:22 +0900

    feat: 응답 헤더 쿠키 추가 #12

---
+++ src/main/java/com/darakthon/healthmatch/WebConfig.java
@@ -0,0 +1,25 @@
+package com.darakthon.healthmatch;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://port-0-healthmatch1-m30h6ofzaa0b4434.sel4.cloudtype.app",
+                                "http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
